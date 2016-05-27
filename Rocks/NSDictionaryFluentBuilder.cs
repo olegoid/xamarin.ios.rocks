41fba18174ffd35fafb97b2734fbc9443b5b8948
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-05-27 15:36:49 +0300

    add fluent builder

---
+++ Rocks/NSDictionaryFluentBuilder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Foundation;
+
+namespace Rocks
+{
+	public class KeyConfig
+	{
+		public ValueConfig<TKey> Key<TKey> (TKey key)
+		{
+			return new ValueConfig<TKey> (key);
+		}
+	}
+
+	public class ValueConfig<TKey>
+	{
+		readonly TKey key;
+
+		public ValueConfig (TKey key)
+		{
+			this.key = key;
+		}
+
+		public KeySetup<TKey, TValue> Value<TValue> (TValue value)
+		{
+			return new KeySetup<TKey, TValue> (new List<Tuple<TKey, TValue>> { new Tuple<TKey, TValue> (key, value) });
+		}
+	}
+
+	public class KeySetup<TKey, TValue>
+	{
+		readonly List<Tuple<TKey, TValue>> list;
+
+		internal KeySetup (List<Tuple<TKey, TValue>> list)
+		{
+			if (list == null)
+				throw new ArgumentNullException (nameof (list));
+			this.list = list;
+		}
+
+		public ValueSetup<TKey, TValue> Key (TKey key)
+		{
+			list.Add (new Tuple<TKey, TValue> (key, default (TValue)));
+			return new ValueSetup<TKey, TValue> (list);
+		}
+
+		// TODO: should return generic dictionary
+		public NSDictionary ToDictionary ()
+		{
+			var keys = list.Select (kvp => NSObject.FromObject (kvp.Item1)).ToArray ();
+			var values = list.Select (kvp => NSObject.FromObject (kvp.Item2)).ToArray ();
+
+			return NSDictionary.FromObjectsAndKeys (values, keys);
+		}
+	}
+
+	public class ValueSetup<TKey, TValue>
+	{
+		readonly List<Tuple<TKey, TValue>> list;
+
+		internal ValueSetup (List<Tuple<TKey, TValue>> list)
+		{
+			if (list == null)
+				throw new ArgumentNullException (nameof (list));
+			this.list = list;
+		}
+
+		public KeySetup<TKey, TValue> Value (TValue value)
+		{
+			var index = list.Count - 1;
+			var tpl = list [index];
+			list [index] = new Tuple<TKey, TValue> (tpl.Item1, value);
+
+			return new KeySetup<TKey, TValue> (list);
+		}
+	}
+
+	public static class NSDictionaryFluentBuilder
+	{
+		public static KeyConfig NSDictionary ()
+		{
+			return new KeyConfig ();
+		}
+	}
+}
---
+++ Tests/NSDictionaryFluentBuilderFixture.cs
@@ -0,0 +1,51 @@
+﻿using Foundation;
+
+using NUnit.Framework;
+using static Rocks.NSDictionaryFluentBuilder;
+
+namespace Tests
+{
+	[TestFixture]
+	public class NSDictionaryFluentBuilderFixture
+	{
+		[Test]
+		public void StringKeyNumberValue ()
+		{
+			NSDictionary dict = NSDictionary ()
+				.Key ("key1").Value (10)
+				.Key ("key2").Value (20)
+				.ToDictionary ();
+
+			var num1 = (NSNumber)dict ["key1"];
+			Assert.True (num1.Int32Value == 10);
+
+			var num2 = (NSNumber)dict ["key2"];
+			Assert.True (num2.Int32Value == 20);
+		}
+
+		[Test]
+		public void StringKeyDictionaryValue ()
+		{
+			var dict1 = NSDictionary ()
+				.Key ("key1").Value ("value1")
+				.Key ("key2").Value ("value2")
+				.ToDictionary ();
+
+			var dict2 = NSDictionary ()
+				.Key ("key10").Value ("value10")
+				.Key ("key20").Value ("value20")
+				.ToDictionary ();
+
+			var result = NSDictionary ()
+				.Key ("dict1").Value (dict1)
+				.Key ("dict2").Value (dict2)
+				.ToDictionary ();
+
+			var resultDict1 = result [(NSString)"dict1"];
+			Assert.True (ReferenceEquals (dict1, resultDict1));
+
+			var resultDict2 = result [(NSString)"dict2"];
+			Assert.True (ReferenceEquals (dict2, resultDict2));
+		}
+	}
+}
